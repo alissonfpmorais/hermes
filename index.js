8eb117e17e41a5a59d218638738a977655f42d1c
alissonfpmorais <alissonfpmorais@gmail.com>
2018-12-20 12:42:40 -0200

    remove components from client api

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const createClient = (params) => {
 
   const client = {
     authenticate: authenticate(conn),
-    components: components(conn),
+    // components: components(conn),
     flows: flows(conn),
     forms: forms(conn),
     personas: personas(conn)
@@ -27,16 +27,26 @@ const createClient = (params) => {
   return client
 }
 
+// function start(params) {
+//   return createClient(params)
+// }
+
+// function call(hermes) {
+//   formBuilder = { "title": "Formulário Padrão", "data": { "sections": [ { "name": "section_188114", "label": "Seção 1", "clientKey": "section_188114", "order": 0, "rows": [ { "name": "section_188114_row_82107", "label": "", "order": 0, "controls": [ { "type": "text", "name": "control_text_701290", "fieldName": "control_text_701290", "label": "Nome", "order": 0, "defaultValue": "", "value": "", "className": "col-md-6", "readonly": false, "labelBold": true, "labelItalic": false, "labelUnderline": false, "required": true, "isMultiLine": false, "isInteger": false, "decimalPlace": 0, "isTodayValue": false, "dateFormat": "dd/mm/yy", "isNowTimeValue": false, "timeFormat": "HH:mm", "isMultiple": false, "isAjax": false, "dataOptions": [], "ajaxDataUrl": "", "isChecked": false } ] } ], "labelPosition": "top", "isDynamic": false, "minInstance": 1, "maxInstance": 0, "instances": [] } ], "layout": "tab", "_uniqueId": 0.8719504664016076 } }
+//
+//   hermes
+//     .authenticate('alissonfpmorais@gmail.com', '1234')
+//     .then(authRes => {
+//       console.log(`Token: ${authRes.data.token}`)
+//
+//       hermes.forms.model
+//         .create(authRes.data.token, formBuilder)
+//         .then(createRes => console.log(createRes))
+//         .catch(createErr => console.log(createErr))
+//     })
+//     .catch(authErr => console.log(authErr))
+// }
+
 module.exports = function (params) {
   return createClient(params)
 }
-
-// const params = {}
-// hermes = require('@holismo/hermes')(params)
-//
-// // object containing `secret` and `duration`
-// // maybe just the token
-// token = hermes.authenticate('', '')
-//
-// // array of component
-// components = hermes.components.list()
